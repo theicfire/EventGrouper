bf2af38ef8acaac8e9a794484dc7ae1fb0a6a22a
Chase <lambertgchase@gmail.com>
2010-11-25 22:02:08 -0500

    Added frontend js file

---
+++ app/webroot/js/frontend_general.js
@@ -0,0 +1,34 @@
+function scheduleToggle() {
+	$(".scheduletoggle").click(function() {
+	if ($('#loggedIn').length != 0) {
+		$( "#dialog-form" ).dialog( "open" );
+		return false;
+	}
+	var eventBlock = $(this).parent().parent(); 
+	var id = eventBlock.attr('id').split("-")[1];
+	var textEl = $(this);
+	if (eventBlock.hasClass('onCalendar')) {
+		$.ajax({url: phpVars.root+"/events/removeFromCalendar/"+id,
+		success: function() {
+			eventBlock.removeClass('onCalendar');
+			eventBlock.addClass('offCalendar');
+			
+			textEl.siblings('.addToSchedule').show();
+			textEl.hide();
+		}
+		});
+	} else {
+		$.ajax({url: phpVars.root+"/events/addToCalendar/"+id,
+		success: function() {
+			eventBlock.removeClass('offCalendar');
+			eventBlock.addClass('onCalendar');
+			textEl.hide();
+			textEl.siblings('.removeFromSchedule').show();				
+			}
+			});
+	
+			
+		}
+		return false;
+	});
+}
